a27f5fa78d6d64c874002aefc142ba0b89b0f0e7
Administrator <l920107@gmail.com>
2026-09-04 09:05:42 +0800

    Add missing #include <QColor> in mtnjob.h and streamline qmake build script

--- src/mtnjob.h
+++ src/mtnjob.h
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QTreeWidgetItem>
 #include <QThread>
 #include <QStandardItem>
+#include <QColor>
 
 #include "mtnworker.h"
 
